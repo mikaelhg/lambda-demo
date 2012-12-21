5e03ab1b44fd96a8bdca8ff5471388706a6aece7
Mikael Gueck <gumi@iki.fi>
2012-12-21 18:02:22 +0200

    add Kotlin support

---
+++ src/main/java/lambda/KotlinDemo.kt
@@ -0,0 +1,23 @@
+package lambda
+
+data class TheWireCharacter(val name : String, vararg val seasons : Int)
+
+val CHARACTERS : List<TheWireCharacter> = arrayListOf(
+        TheWireCharacter("Jimmy McNulty", 1, 2, 3, 4, 5),
+        TheWireCharacter("Lester Freamon", 2, 3, 4, 5),
+        TheWireCharacter("Stringer Bell", 1, 2, 3),
+        TheWireCharacter("Prez", 3, 4),
+        TheWireCharacter("Omar Little", 3, 4, 5),
+        TheWireCharacter("Chris Partlow", 5),
+        TheWireCharacter("Frank Sobotka", 2),
+        TheWireCharacter("D'Angelo Barksdale", 1, 2),
+        TheWireCharacter("Avon Barksdale", 1, 2, 3)
+)
+
+fun main(args : Array<String>) {
+    val docks = CHARACTERS.filter { it.seasons.toList().contains(2) }
+                          .sortBy { it.seasons.toList().size }
+                          .map { it.name }
+
+    println("Characters in the Baltimore docks-centered season: " + docks)
+}
