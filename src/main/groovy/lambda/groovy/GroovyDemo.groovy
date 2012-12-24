5f961605f1c68ff8d8604562155ebe15d55fa0a3
Mikael Gueck <gumi@iki.fi>
2012-12-24 18:13:46 +0200

    make the Groovy example resemble the others more

--- src/main/groovy/lambda/groovy/GroovyDemo.groovy
+++ src/main/groovy/lambda/groovy/GroovyDemo.groovy
@@ -1,27 +1,26 @@
 package lambda.groovy
 
-/**
- * http://en.wikipedia.org/wiki/List_of_The_Wire_characters
- */
-class TheWireCharacter {
-    String name;
-    Set<Integer> seasons;
+import groovy.transform.*
+
+@Immutable @TupleConstructor class TheWireCharacter {
+    final String name;
+    final Set<Integer> seasons;
 }
 
-def CHARACTERS = [
-        new TheWireCharacter(name: "Jimmy McNulty", seasons: [1, 2, 3, 4, 5] as Set),
-        new TheWireCharacter(name: "Lester Freamon", seasons: [2, 3, 4, 5] as Set),
-        new TheWireCharacter(name: "Stringer Bell", seasons: [1, 2, 3] as Set),
-        new TheWireCharacter(name: "Prez", seasons: [3, 4] as Set),
-        new TheWireCharacter(name: "Omar Little", seasons: [3, 4, 5] as Set),
-        new TheWireCharacter(name: "Chris Partlow", seasons: [5] as Set),
-        new TheWireCharacter(name: "Frank Sobotka", seasons: [2] as Set),
-        new TheWireCharacter(name: "D'Angelo Barksdale", seasons: [1, 2] as Set),
-        new TheWireCharacter(name: "Avon Barksdale", seasons: [1, 2, 3] as Set)
-]
+final CHARACTERS = [
+        new TheWireCharacter("Jimmy McNulty", [1, 2, 3, 4, 5] as Set),
+        new TheWireCharacter("Lester Freamon", [2, 3, 4, 5] as Set),
+        new TheWireCharacter("Stringer Bell", [1, 2, 3] as Set),
+        new TheWireCharacter("Prez", [3, 4] as Set),
+        new TheWireCharacter("Omar Little", [3, 4, 5] as Set),
+        new TheWireCharacter("Chris Partlow", [5] as Set),
+        new TheWireCharacter("Frank Sobotka", [2] as Set),
+        new TheWireCharacter("D'Angelo Barksdale", [1, 2] as Set),
+        new TheWireCharacter("Avon Barksdale", [1, 2, 3] as Set)
+].asImmutable()
 
-def docks = CHARACTERS.findAll { it.seasons.contains(2) }
-                      .sort { it.seasons.size() }
-                      .collect { it.name }
+final docks = CHARACTERS.findAll { it.seasons.contains(2) }
+                        .sort { it.seasons.size() }
+                        .collect { it.name }
 
 println("Characters in the Baltimore docks-centered season: " + docks)
