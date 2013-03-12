39a1fdf7c00e00de9493f8cf335baedbb5570644
Mikael Gueck <gumi@iki.fi>
2013-03-12 19:27:18 +0200

    Yes, this would be very unwise in production code.

--- src/test/java/lambda/JavaDocksTest.java
+++ src/test/java/lambda/JavaDocksTest.java
@@ -1,42 +1,35 @@
 package lambda;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
-import static org.mockito.Mockito.*;
 
 /**
  * You shouldn't test System.out like this. This is very bad. The only reason
  * I'm doing it like this is because this isn't production code, but concept
- * demonstration. DON'T DO LIKE I DO, DO WHAT I SAY!
+ * demonstration. DON'T DO WHAT I DO, DO WHAT I SAY!
  */
 public class JavaDocksTest {
 
     private static final String EXPECTED =
-            "Characters in the Baltimore docks-centered season: [Frank Sobotka, D'Angelo Barksdale, Stringer Bell, Avon Barksdale, Lester Freamon, Jimmy McNulty]";
+            "Characters in the Baltimore docks-centered season: [Frank Sobotka, D'Angelo Barksdale, Stringer Bell, Avon Barksdale, Lester Freamon, Jimmy McNulty]\n";
 
     private final static String[] EMPTY_ARRAY = new String[0];
 
-    private PrintStream realOut;
-
-    @Before
-    public void saveOut() {
-        realOut = System.out;
-        System.setOut(mock(PrintStream.class));
-    }
-
-    @After
-    public void retrieveOut() {
-        System.setOut(realOut);
-    }
-
     @Test
-    public void testKotlin() {
-        lambda.kotlin.KotlinPackage.main(EMPTY_ARRAY);
-        verify(System.out).println(EXPECTED);
+    public void testKotlin() throws Exception {
+        final PrintStream oldOut = System.out;
+        try (final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             final PrintStream newOut = new PrintStream(baos))
+        {
+            System.setOut(newOut);
+            lambda.kotlin.KotlinPackage.main(EMPTY_ARRAY);
+            Assert.assertEquals(baos.toString("UTF-8"), EXPECTED);
+        } finally {
+            System.setOut(oldOut);
+        }
     }
 
 }
